9f1b2fcf55fb728315dca3b3fe3dd9bd99def2c9
Seo Myunggyun (Jonathan) <tkiapril@users.noreply.github.com>
2022-11-09 13:48:32 +0900

    Adapt endpoint summary page

--- pages/[endpoint].tsx
+++ pages/[endpoint].tsx
@@ -1,37 +1,83 @@
 import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { Checkbox, Pivot, PivotItem } from '@fluentui/react';
 
-import {
-  Checkbox,
-  Pivot,
-  PivotItem,
-} from '@fluentui/react';
+import { BlockList, TransactionList } from 'components/List';
+import OffsetSwitch from 'components/OffsetSwitch';
+
+import { listTxColumns, mainMineColumns } from 'lib/listColumns';
+import useOffset, { limit } from 'lib/useOffset';
 
 import {
   Block,
-  BlockListComponent,
+  BlockListDocument,
+  BlockListQuery,
   Transaction,
-  TransactionListComponent,
-} from '../generated/graphql';
+  TransactionListDocument,
+  TransactionListQuery,
+} from 'src/gql/graphql';
 
-import useOffset, { limit } from '../misc/useOffset';
-import { listTxColumns, mainMineColumns } from '../misc/columns';
-
-import { BlockList, TransactionList } from '../components/List';
-import OffsetSwitch from '../components/OffsetSwitch';
-
-import { IndexPageProps } from '../pages/index';
-
-type ListPageProps = IndexPageProps;
+import { ExplorerPageProps } from 'pages/_app';
 
 const POLL_INTERVAL = 2000;
 const ROUND_DIGITS = 4;
 
-const ListPage: React.FC<ListPageProps> = ({ location, ...props }) => {
-  const [offset, olderHandler, newerHandler] = useOffset(location);
+export default function Summary({ endpoint, asPath }: ExplorerPageProps) {
+  let blocks = null,
+    blockList,
+    transactions = null,
+    transactionList;
+  const [offset, olderHandler, newerHandler] = useOffset(asPath);
   const [excludeEmptyTxs, setExcludeEmptyTxs] = useState(false);
-  const [blocks, setBlocks] = useState<Block[] | null>(null);
-  const [blocksLoading, setBlocksLoading] = useState(false);
-  const [transactionsLoading, setTransactionsLoading] = useState(false);
+  const {
+    loading: blocksLoading,
+    error: blocksError,
+    data: blocksData,
+  } = useQuery<BlockListQuery>(BlockListDocument, {
+    variables: { offset, limit, excludeEmptyTxs },
+    pollInterval: POLL_INTERVAL,
+  });
+  const {
+    loading: transactionsLoading,
+    error: transactionsError,
+    data: transactionsData,
+  } = useQuery<TransactionListQuery>(TransactionListDocument, {
+    variables: { offset, limit, desc: true },
+    pollInterval: POLL_INTERVAL,
+  });
+  if (blocksError) {
+    console.error(blocksError);
+    blockList = <p>{blocksError.message}</p>;
+  } else {
+    if (!blocksLoading) {
+      blocks = blocksData?.chainQuery.blockQuery?.blocks as Block[] | null;
+    }
+    blockList = (
+      <BlockList
+        blocks={blocks}
+        loading={blocksLoading}
+        columns={mainMineColumns(endpoint)}
+        endpoint={endpoint}
+      />
+    );
+  }
+  if (transactionsError) {
+    console.error(transactionsError);
+    transactionList = <p>{transactionsError.message}</p>;
+  } else {
+    if (!transactionsLoading) {
+      transactions = transactionsData?.chainQuery.transactionQuery
+        ?.transactions as Transaction[] | null;
+    }
+    transactionList = (
+      <TransactionList
+        columns={listTxColumns(endpoint)}
+        endpoint={endpoint}
+        loading={transactionsLoading}
+        transactions={transactions}
+      />
+    );
+  }
   return (
     <main>
       <Checkbox
@@ -49,83 +95,14 @@ const ListPage: React.FC<ListPageProps> = ({ location, ...props }) => {
         }}
       />
       <Pivot>
-        <PivotItem headerText='Blocks'>
-          <BlockListComponent
-            variables={{ offset, limit, excludeEmptyTxs }}
-            pollInterval={POLL_INTERVAL}>
-            {({ data, loading, error }) => {
-              setBlocksLoading(loading);
-              if (error) {
-                console.error(error);
-                return <p>{error.message}</p>;
-              }
-
-              if (!loading) {
-                setBlocks(
-                  data &&
-                    data.chainQuery.blockQuery &&
-                    data.chainQuery.blockQuery.blocks
-                    ? (data.chainQuery.blockQuery.blocks as Block[])
-                    : null
-                );
-              }
-
-              return (
-                <BlockList
-                  blocks={blocks}
-                  loading={loading}
-                  columns={(mainMineColumns(props.pageContext.endpoint.name))}
-                  endpointName={props.pageContext.endpoint.name}
-                />
-              );
-            }}
-          </BlockListComponent>
-        </PivotItem>
-        <PivotItem headerText='Transactions'>
-          <TransactionListComponent
-            variables={{ offset, limit, desc: true }}
-            pollInterval={POLL_INTERVAL}>
-            {({ data, loading, error }) => {
-              setTransactionsLoading(loading);
-              if (error) {
-                console.error(error);
-                return <p>{error.message}</p>;
-              }
-
-              let transactions = null;
-              if (!loading) {
-                transactions =
-                  data &&
-                  data.chainQuery.transactionQuery &&
-                  data.chainQuery.transactionQuery.transactions
-                    ? (data.chainQuery.transactionQuery
-                        .transactions as Transaction[])
-                    : null;
-              }
-
-              return (
-                <TransactionList
-                  columns={listTxColumns(props.pageContext.endpoint.name)}
-                  endpointName={props.pageContext.endpoint.name}
-                  loading={loading}
-                  transactions={transactions}
-                />
-              );
-            }}
-          </TransactionListComponent>
-        </PivotItem>
+        <PivotItem headerText="Blocks">{blockList}</PivotItem>
+        <PivotItem headerText="Transactions">{transactionList}</PivotItem>
       </Pivot>
     </main>
   );
-};
-
-export default ListPage;
-
-export interface SummaryCardsProps {
-  blocks: Block[] | null;
 }
 
-const SummaryCards: React.FC<SummaryCardsProps> = ({ blocks }) => {
+function SummaryCards({ blocks }: { blocks: Block[] | null }) {
   if (blocks === null)
     return <Cards interval={0} difficultyAverage={0} totalTxNumber={0} />;
 
@@ -150,31 +127,31 @@ const SummaryCards: React.FC<SummaryCardsProps> = ({ blocks }) => {
       totalTxNumber={totalTxNumber}
     />
   );
-};
-
-interface CardsProps {
-  interval: number;
-  difficultyAverage: number;
-  totalTxNumber: number;
 }
 
-const Cards: React.FC<CardsProps> = ({
+function Cards({
   interval,
   difficultyAverage,
   totalTxNumber,
-}) => (
-  <div className="cards">
-    <div className="card" key="interval">
-      <strong>{interval.toFixed(ROUND_DIGITS)}</strong> sec
-      <p>Average interval in this page</p>
+}: {
+  interval: number;
+  difficultyAverage: number;
+  totalTxNumber: number;
+}) {
+  return (
+    <div className="cards">
+      <div className="card" key="interval">
+        <strong>{interval.toFixed(ROUND_DIGITS)}</strong> sec
+        <p>Average interval in this page</p>
+      </div>
+      <div className="card" key="difficultyAverage">
+        <strong>{Math.floor(difficultyAverage).toLocaleString()}</strong>
+        <p>Average difficulty in this page</p>
+      </div>
+      <div className="card" key="total-tx-number">
+        <strong>{Math.floor(totalTxNumber).toLocaleString()}</strong>
+        <p>Total txs in this page</p>
+      </div>
     </div>
-    <div className="card" key="difficultyAverage">
-      <strong>{Math.floor(difficultyAverage).toLocaleString()}</strong>
-      <p>Average difficulty in this page</p>
-    </div>
-    <div className="card" key="total-tx-number">
-      <strong>{Math.floor(totalTxNumber).toLocaleString()}</strong>
-      <p>Total txs in this page</p>
-    </div>
-  </div>
-);
+  );
+}
